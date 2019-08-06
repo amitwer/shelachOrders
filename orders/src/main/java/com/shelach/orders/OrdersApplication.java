c372b1b65ccd63e76c81a8591652a304b38a0f71
Amit <12250123+amitwer@users.noreply.github.com>
2019-08-06 08:39:21 +0300

    static analysis fixes

--- orders/src/main/java/com/shelach/orders/OrdersApplication.java
+++ orders/src/main/java/com/shelach/orders/OrdersApplication.java
@@ -3,6 +3,7 @@ package com.shelach.orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("WeakerAccess")
 @SpringBootApplication
 public class OrdersApplication {
 
