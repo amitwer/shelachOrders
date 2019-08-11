8a90b598e4a644e7aa53a9124c004a0a87eb8fdc
Amit <12250123+amitwer@users.noreply.github.com>
2019-08-11 14:21:46 +0300

    add dummy ordersService

--- orders/src/main/java/com/shelach/orders/controllers/OrdersController.java
+++ orders/src/main/java/com/shelach/orders/controllers/OrdersController.java
@@ -1,6 +1,8 @@
 package com.shelach.orders.controllers;
 
+import com.shelach.orders.services.ProductsService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -9,7 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
-public class OrdersController {
+class OrdersController {
+    private final ProductsService productsService;
+
+    @Autowired
+    public OrdersController(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
     @SuppressWarnings("SameReturnValue")
     @RequestMapping("")
     public String mainPage(ExtendedModelMap model) {
@@ -17,6 +26,7 @@ public class OrdersController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
         model.put("username", username);
+        model.put("products", productsService.getProducts(username));
         log.info("Orders page ready for user <{}>", username);
         return "orders";
     }
